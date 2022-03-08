e467a50f1873d41e139cebfbee7695211b32b214
Johnson <j3.soon@msa.hinet.net>
2022-03-08 12:03:20 +0800

    Fix Dev Server contentBase error

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -56,7 +56,9 @@ module.exports = {
         })
     ],
     devServer: {
-        contentBase: path.join(__dirname, 'dist'),
+        static: {
+            directory: path.join(__dirname, 'dist'),
+        },
         compress: true,
         port: 9000,
     },
--- webpack.dist.config.js
+++ webpack.dist.config.js
@@ -55,7 +55,9 @@ module.exports = {
         })
     ],
     devServer: {
-        contentBase: path.join(__dirname, 'dist'),
+        static: {
+            directory: path.join(__dirname, 'dist'),
+        },
         compress: true,
         port: 9000,
     },
