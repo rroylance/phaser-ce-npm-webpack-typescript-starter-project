f359b86b5106c5a9cc0afeda444beb04fe5e6015
Johnson <j3.soon@msa.hinet.net>
2022-03-08 12:01:50 +0800

    Fix Dev Server inline error

--- webpack.dist.config.js
+++ webpack.dist.config.js
@@ -58,12 +58,11 @@ module.exports = {
         contentBase: path.join(__dirname, 'dist'),
         compress: true,
         port: 9000,
-        inline: true,
-        watchOptions: {
-            aggregateTimeout: 300,
-            poll: true,
-            ignored: /node_modules/
-        }
+    },
+    watchOptions: {
+        aggregateTimeout: 300,
+        poll: true,
+        ignored: /node_modules/
     },
     module: {
         rules: [
